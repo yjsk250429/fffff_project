3ba4fad06e5676b46009f36d155811df15d8f9ba
SohnSangbum <foot0707@gmail.com>
2025-08-14 18:54:38 +0900

    상범 수정 250814 PM 18:54

--- src/components/collection/CollectionProductItem.jsx
+++ src/components/collection/CollectionProductItem.jsx
@@ -5,33 +5,28 @@ import { Link } from 'react-router-dom';
 const CollectionProductItem = ({ product }) => {
     return (
         <li>
-            <Link to=''>
-            <div className="img-wrap">
-                <img src="" alt="" />
-                {/* <i>
+            <Link to="">
+                <div className="img-wrap">
+                    <img src="" alt="" />
+                    {/* <i>
                     <IoHeartOutline />
                     <IoHeart />
                 </i> */}
-            </div>
-            <div className="text-wrap">
-                <p className="product-info">
-<<<<<<< HEAD
-                    <strong>
-                        아몬드 딜리셔스 핸드크림 <br />
-                        아몬드 딜리셔스 핸드크림
-                    </strong>
-=======
-                    <strong>아몬드 딜리셔스 핸드크림
->>>>>>> d58c1bbb0bf75eea41c80d99c5cc4d97e21c01ad
-                    <span>★ 5.0</span>
+                </div>
+                <div className="text-wrap">
+                    <p className="product-info">
+                        <strong>아몬드 딜리셔스 핸드크림 아몬드 딜리셔스 핸드크림</strong>
 
-                    </strong>
-                    <em>NO.1 핸드크림</em>
-                </p>
-                <p className="price-info">
-                    <span>150ml / 41,000원</span><i><PiHandbagSimple /></i>
-                </p>
-            </div>
+                        <span>★ 5.0</span>
+                        <em>NO.1 핸드크림</em>
+                    </p>
+                    <p className="price-info">
+                        <span>150ml / 41,000원</span>
+                        <i>
+                            <PiHandbagSimple />
+                        </i>
+                    </p>
+                </div>
             </Link>
         </li>
     );
