d0149c210a47ca1b73a7ef0bcf6c28a3a8d64166
gkstmftt <yanglily328@gmail.com>
2025-08-14 19:03:11 +0900

    한슬0814

--- src/components/event/content1/Gift_3setList.jsx
+++ src/components/event/content1/Gift_3setList.jsx
@@ -1,9 +1,10 @@
 import Gift_3setItem from './Gift_3setItem';
 import gift3setData from '../../../assets/api/gift3setData';
+import { Gift_3setStyle } from './style';
 
 const Gift_3setList = () => {
     return (
-        <div>
+        <Gift_3setStyle>
             <strong>공식몰에서 제공하는 특별 사은품 3종 세트를 만나보세요.</strong>
             <ul>
                 {gift3setData.map((gift) => (
@@ -11,7 +12,7 @@ const Gift_3setList = () => {
                 ))}
             </ul>
             <img src="/images/event/event0.png" alt="사은품3종세트" />
-        </div>
+        </Gift_3setStyle>
     );
 };
 
--- src/components/event/content1/style.js
+++ src/components/event/content1/style.js
@@ -2,5 +2,82 @@ import styled from 'styled-components';
 
 export const Gift_3setStyle = styled.div`
     height: 1100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    /* justify-content: center; */
+    box-sizing: border-box;
+    position: relative;
+    text-align: center;
+    strong {
+        font-size: 30px;
+        font-weight: 600;
+        display: block;
+        margin-top: 100px;
+    }
+    ul {
+        li {
+            position: absolute;
+            &::after {
+                content: '';
+                width: 1px;
+                height: 84px;
+                background: #000;
+                position: absolute;
+                bottom: -100px;
+            }
+            &:nth-of-type(1) {
+                left: 630px;
+                top: 275px;
+            }
+            &:nth-of-type(2) {
+                bottom: 100px;
+                left: 845px;
+
+                &::after {
+                    content: '';
+                    width: 1px;
+                    height: 84px;
+                    background: #000;
+                    position: absolute;
+                    top: -100px;
+                }
+            }
+            &:nth-of-type(3) {
+                top: 275px;
+                right: 610px;
+                height: 106px;
+                &::after {
+                    height: 106px;
+                    bottom: -140px;
+                }
+            }
+        }
+    }
+    img {
+        margin-top: 260px;
+        width: 752px;
+        height: 463px;
+    }
 `;
-export const Gift_3setItemStyle = styled.li``;
+export const Gift_3setItemStyle = styled.li`
+    em {
+        color: var(--sub, #00274c);
+        font-size: 24px;
+        font-weight: 500;
+        display: block;
+        margin-bottom: 14px;
+        line-height: 1;
+    }
+    p {
+        font-size: 14px;
+        line-height: 1.2;
+
+        span {
+            display: block;
+            &:nth-child(2n) {
+                text-align: center;
+            }
+        }
+    }
+`;
