5d881f0032ce00ad320dee4e3693bbb975b4c460
SohnSangbum <foot0707@gmail.com>
2025-08-20 16:08:12 +0900

    상범 수정 250820 PM 04:08

--- src/components/wishlist/WishEmpty.jsx
+++ src/components/wishlist/WishEmpty.jsx
@@ -12,8 +12,8 @@ const WishEmpty = () => {
             <div className="inner">
                 <h2>위시리스트가 비어있습니다.</h2>
                 <em>마음에 드는 제품을 담아보세요</em>
-                <p onClick={onGo}>
-                    <Button text="쇼핑하러 가기" bgColor="#000" textColor="#fff" />
+                <p>
+                    <Button onClick={onGo} text="쇼핑하러 가기" bgColor="#000" textColor="#fff" />
                 </p>
             </div>
         </WishEmptyStyle>
